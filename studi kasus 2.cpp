8ae3f2f85ecee2e1bea42139dca675fbf78587ce
KrisnaCakraNingrat <krisna2100018449@webmail.uad.ac.id>
2022-04-01 17:33:38 +0700

    Studi Kasus 2 Baruuu

--- studi kasus 2.cpp
+++ studi kasus 2.cpp
@@ -1,3 +1,9 @@
+/*
+  KRISNA CAKRA NINGRAT (2100018449)
+  ANELA INDRA (2100018454)
+  SASQIA AULIA NUR AINI (2100018456)
+*/
+
 #include <iostream>
 #include <conio.h>
 using namespace std;
@@ -61,4 +67,66 @@ int main (){
 			cout << "Menu yang Anda pilih tidak ada!";
 	}
 	
+	cout << "Masukkan pilihan jarak rumah anda (KM) : ";
+	cin >> jarak;
+	cout << endl;
+	if ( jarak<3){
+		cout << "Anda mendapat ongkir sebanyak 15000\n";
+		ongkir = 15000;
+		cout << "Total ongkir = "<< ongkir;
+	}
+	else if (jarak>=3  ) {
+		cout << "Anda mendapat ongkir sebanyak 25000\n";
+		ongkir = 25000;
+		cout << "Total ongkir = " << ongkir;	
+	}
+	else {
+		cout<<"!!!";
+	}
+	
+	cout <<"\nTotal = " << total << endl;
+	cout << "===========================\n";
+	
+	if (total > 25000 && total <= 50000){
+		cout << "Potongan biaya ongkir 3000" << endl;
+		pot_ong = 3000;
+		hasil_pot = ongkir - pot_ong;
+		potbel = 0 ;
+		totbel = total - potbel;
+		akhir = total-potbel + hasil_pot;
+	}
+	else if (total > 50000 && total <= 150000){
+		cout<< "Potongan biaya ongkir 5000 dan diskon 15%" << endl;
+		pot_ong = 5000;
+		hasil_pot = ongkir - pot_ong;
+		potbel = total * 0.15;
+		totbel = total - potbel;
+		akhir = total - potbel + hasil_pot;
+	}
+	else if (total > 150000){
+		cout << "Potongan biaya ongkir 8000 dan diskon 35%" << endl;
+		pot_ong = 8000;
+		hasil_pot = ongkir - pot_ong;
+		potbel = total * 0.35;
+		totbel = total - potbel;
+		akhir = total - potbel + hasil_pot;
+	}
+	else {
+		cout << "Tidak mendapat potongan!";
+	}
+	
+	cout << "\n\n++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+	cout << "|                 STRUK PEMBAYARAN               |" << endl;
+	cout << "++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+	cout << "Pilihan Menu\t: " << "\n" << menu << endl;
+	cout << jumlah << " x " << harga << "\t\t: Rp. " << total << endl;
+	cout << "===================================================" << endl;
+	cout << "JUMLAH\t\t\t: Rp. " << total << endl;
+	cout << "ONGKIR\t\t\t: Rp. " << ongkir << endl;
+	cout << "POTONGAN BELANJA\t: Rp. " << potbel << endl;
+	cout << "POTONGAN ONGKIR\t\t: Rp. " << pot_ong << endl;
+	cout << "---------------------------------------------------" << endl;
+	cout << "TOTAL\t\t\t: Rp. " << akhir << endl;
+	cout << "===================================================" << endl;
+	return 0;
 }
