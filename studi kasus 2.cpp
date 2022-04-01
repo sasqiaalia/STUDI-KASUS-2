4079e45c73325a99fca3ba5478fa2efb71944ec9
sasqiaaulianuraini456 <sasqia210001456@webmail.uad.ac.id>
2022-04-01 16:10:23 +0700

    studi kasus 2

---
+++ studi kasus 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main (){
+	int pilih, ongkir, jumlah, total, harga, potbel, totbel, pot_ong, hasil_pot, akhir;
+	string menu;
+	float jarak;
+	cout << "++++++++++++++++++++++++++++++++++++++++++++++++\n";
+	cout << "|          STORMI CHICKEN AND SEAFOOD          |\n";
+	cout << "++++++++++++++++++++++++++++++++++++++++++++++++\n";
+	
+	cout << "Pilihan Menu : \n";
+	cout << "1.Ayam geprek\t: 21000" <<endl;
+	cout << "2.Ayam goreng\t: 17000" <<endl;
+	cout << "3.Udang goreng\t: 19000" << endl;
+    cout << "4.Cumi goreng\t: 20000" << endl;
+    cout << "5.Ayam bakar\t: 25000" << endl;
+	cout << "Masukkan menu pilihan : ";
+	cin >> pilih;
+	cout << endl;
+}
